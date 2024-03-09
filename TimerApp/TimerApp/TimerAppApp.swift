5ed122fa893e72b90d66ba50eccf24734779314e
Naveenkumar-1998 <156515529+Naveenkumar-1998@users.noreply.github.com>
2024-03-09 12:52:24 +0530

    Project Initialised
    In this commit:
    -> TimerApp project created and added into the repository.

---
+++ TimerApp/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Softasuave on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
